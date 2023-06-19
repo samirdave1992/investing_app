d1b161476c025dde90e3b60fc3882b74d82b5b8e
Samir Dave <samirdave1992@gmail.com>
2023-06-19 10:35:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ if page=="Top Investor's holdings":
 
                 df=pd.read_html(c,match='Stock')
                 df=df[0]
-                df['Ticker']=df.Stock.str.extract('(.*)- ', expand=False)
+            #    df['Ticker']=df.Stock.str.extract('(.*)- ', expand=False)
             #   df=df[["Stock","Ticker","% ofPortfolio","RecentActivity","Shares","ReportedPrice*","Value","CurrentPrice","+/-ReportedPrice","52WeekLow","52WeekHigh"]]
               #  df['Ticker'] = df['Ticker'].str.replace('.','-')
 
